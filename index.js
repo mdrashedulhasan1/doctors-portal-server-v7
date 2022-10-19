515c483f8f229b92f10e62ebf0b509d219831dcc
unknown <rashedul15-1718@diu.edu.bd>
2022-10-19 02:41:55 -0700

    Server Side

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k07wrg5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const serviceCollection = client.db("doctors_portal").collection("services");
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send('Hello Rashedul!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
