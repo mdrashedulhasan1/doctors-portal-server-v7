4c2a6bb50d995671d5b2d24f2c001b842adb5e0b
unknown <rashedul15-1718@diu.edu.bd>
2022-11-08 09:48:43 -0800

    Final Code

--- index.js
+++ index.js
@@ -11,11 +11,22 @@ const client = new MongoClient(uri);
 async function run() {
     try {
         const serviceCollection = client.db("doctors_portal").collection("services");
+        const bookingCollection = client.db("doctors_portal").collection("bookings");
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray()
             res.send(services)
+        });
+        app.post('/booking',async(req,res)=>{
+            const booking = req.body;
+            const query = {treatment:booking.treatment, date:booking.date, patient:booking.patient}
+            const exists = await bookingCollection.findOne(query);
+            if(exists){
+                return res.send({success:false, booking:exists})
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({success:true, result});
         })
     } finally {
         //   await client.close();
